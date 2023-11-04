10bee8adbe51cee12538318db2090e8d5ca4727e
Soumay Gupta <118595898+soumayg@users.noreply.github.com>
2023-11-04 22:14:19 +0530

    static folder path

--- tourism/settings.py
+++ tourism/settings.py
@@ -93,7 +93,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
